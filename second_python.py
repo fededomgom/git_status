bf33837ee1c0aace315ee3282fe48f04817b14c5
fededomgom <fededomgom@gmail.com>
2021-06-12 21:05:18 -0300

    Agregué dos archivos de python, y 1 de html

---
+++ second_python.py
@@ -0,0 +1,7 @@
+print("Chancho")
+def i_love_git():
+    print("MI MAMÁ ME DIO LA VIDA, GIT LAS GANAS DE VIVIRLA")
+
+for i in range(0,100):
+    i_love_git()
+
---
+++ third_python.py
@@ -0,0 +1,12 @@
+while True:
+    while True:
+        eleccion = input("Fua, aguante VIM. Pato o hamburguesa?")
+        if eleccion == "Pato":
+            print("Cuack")
+            break
+        elif eleccion == "Hamburguesa":
+            print("ñam")
+            break
+        else:
+            print("No es tan dificil che")
+
